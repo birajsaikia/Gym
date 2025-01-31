0e0ab7fda40477cc4d8da635dddeeb8aad16e859
unknown <birajjyo2@gmail.com>
2025-02-01 01:13:15 +0530

    first commit

--- Golds-Gym/src/components/BodyParts.jsx
+++ Golds-Gym/src/components/BodyParts.jsx
@@ -1,37 +1,44 @@
-import { Stack, Typography } from "@mui/material";
-import React from "react";
-import Icon from "../assets/icons/gym.png";
+import { Stack, Typography } from '@mui/material';
+import React from 'react';
+import Icon from '../assets/icons/gym.png';
 
 const BodyParts = ({ item, setBodyPart, bodyPart }) => {
   return (
     <Stack
       type="button"
-      alignItems={"center"}
-      justifyContent={"center"}
+      alignItems={'center'}
+      justifyContent={'center'}
       className="bodyPart-card"
       sx={{
-        borderTop: bodyPart === item ? "4px solid #ff2625" : "",
-        backgroundColor: "#fff",
-        borderBottomLeftRadius: "20px",
-        width: "270px",
-        height: "280px",
-        cursor: "pointer",
-        gap: "47px",
+        borderTop: bodyPart === item ? '4px solid #ff2625' : '',
+        backgroundColor: '#fff',
+        borderBottomLeftRadius: '20px',
+        width: '270px',
+        height: '280px',
+        cursor: 'pointer',
+        gap: '47px',
       }}
-      onClick={()=>{
+      onClick={() => {
         setBodyPart(item);
-        window.scrollTo({top:'1800',left:100,behavior:'smooth'})
+        window.scrollTo({ top: '1800', left: 100, behavior: 'smooth' });
       }}
     >
       <img
         src={Icon}
         alt="gym"
         style={{
-          width: "40px",
-          height: "40px",
+          width: '40px',
+          height: '40px',
         }}
       />
-      <Typography fontSize={'24px'} fontWeight={'bold'} color={'#3A1212'} textTransform={'capitalize'}>{item}</Typography>
+      <Typography
+        fontSize={'24px'}
+        fontWeight={'bold'}
+        color={'#3A1212'}
+        textTransform={'capitalize'}
+      >
+        {item}
+      </Typography>
     </Stack>
   );
 };
